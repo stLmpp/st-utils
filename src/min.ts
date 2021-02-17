3b9f9e561212f5c2bf3d7d6049622f2feb67e892
stLmpp <gui.stlmpp@hotmail.com>
2021-02-16 22:47:39 -0300

    - added max and min functions for number primitive arrays - fixed a bug where the value was compared wrongly in the maxBy and minBy functions

--- src/min.ts
+++ src/min.ts
@@ -1,5 +1,9 @@
 import type { ConditionalKeys } from 'type-fest';
 
+export function min(array: number[]): number {
+  return Math.min(...array);
+}
+
 export function minBy<T extends Record<any, any>, K extends ConditionalKeys<T, number>>(array: T[], key: K): T {
-  return array.reduce((acc, item) => (item[key] < acc ? item : acc), array[0]);
+  return array.reduce((acc, item) => (item[key] < acc[key] ? item : acc), array[0]);
 }
