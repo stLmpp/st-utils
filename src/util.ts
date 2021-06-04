3c3adef5a0000bb1589ac05c13ae5f2ab824edcf
stLmpp <gui.stlmpp@hotmail.com>
2021-06-04 15:40:21 -0300

    - added function isObjectEqualShallow - added function getFirstKey - updated dependencies

--- src/util.ts
+++ src/util.ts
@@ -68,3 +68,35 @@ export function normalizeString(str: string): string {
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '');
 }
+
+export function isObjectEqualShallow(
+  objectA: Record<any, any> | null | undefined,
+  objectB: Record<any, any> | null | undefined
+): boolean {
+  if (objectA === objectB) {
+    return true;
+  }
+  if (!objectA || !objectB) {
+    return false;
+  }
+  const keysA = Object.keys(objectA);
+  const keysB = Object.keys(objectB);
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+  for (const key of keysA) {
+    if (objectA[key] !== objectB[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function getFirstKey<T extends Record<any, any>>(object: T): keyof T | undefined {
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      return key;
+    }
+  }
+  return undefined;
+}
