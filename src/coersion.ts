fa4d47d23c1a284a8af9385cae919cc6af6391d1
stLmpp <gui.stlmpp@hotmail.com>
2021-08-12 10:57:56 -0300

    - added type NumberInput - added function coerceNumberProperty

--- src/coersion.ts
+++ src/coersion.ts
@@ -1,4 +1,5 @@
-import { isArray, isNil } from './util';
+import { isArray, isNil, isNumber } from './util';
+import { BooleanInput, NumberInput } from './type';
 
 export function coerceArray<T>(value: T | T[]): T[];
 export function coerceArray<T>(value: T | readonly T[]): readonly T[];
@@ -6,6 +7,17 @@ export function coerceArray<T>(value: T | T[]): T[] {
   return isArray(value) ? value : [value];
 }
 
-export function coerceBooleanProperty(value: any): boolean {
+export function coerceBooleanProperty(value: BooleanInput): boolean {
   return !isNil(value) && `${value}` !== 'false';
+}
+
+export function coerceNumberProperty(value: NumberInput, fallback = 0): number {
+  if (isNumber(value)) {
+    return value;
+  }
+  if (isNil(value)) {
+    return fallback;
+  }
+  const parsed = parseFloat(value);
+  return isNaN(parsed) ? fallback : parsed;
 }
--- src/type.ts
+++ src/type.ts
@@ -1,2 +1,3 @@
 export type KeyType = string | number | symbol;
 export type BooleanInput = string | boolean | null | undefined;
+export type NumberInput = string | number | null | undefined;
