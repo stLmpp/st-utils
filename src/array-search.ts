815c4b2a0b19f011ac350ff41c0f98dda57d5a1b
stLmpp <gui.stlmpp@hotmail.com>
2021-08-12 15:13:30 -0300

    - arraySearch now accepts null and undefined in the term argument

--- src/array-search.ts
+++ src/array-search.ts
@@ -3,10 +3,13 @@ import { isArray, isFunction, normalizeString } from './util';
 export function arraySearch<T, K extends keyof T>(
   array: T[],
   keyOrKeysOrCallback: K | K[] | ((item: T) => T[K]),
-  term: string
+  _term: string | null | undefined
 ): T[] {
+  if (!_term) {
+    return array;
+  }
   let predicate: (entity: T) => boolean;
-  term = normalizeString(term).toLowerCase();
+  const term = normalizeString(_term).toLowerCase();
   if (isFunction(keyOrKeysOrCallback)) {
     predicate = entity => normalizeString(keyOrKeysOrCallback(entity)).toLowerCase().includes(term);
   } else if (isArray(keyOrKeysOrCallback)) {
